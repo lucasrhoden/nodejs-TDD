603658f5e0ec5da8fb565410ec6be2dac6e28aab
Lucas Rhoden <lucas.rhoden@academicv.com>
2022-07-30 13:13:44 +0100

    Create typescript.config

--- src/index.js
+++
@@ -1,8 +0,0 @@
-class Person {
-    speak(name) {
-        return `Hello, ${name?.toUpperCase() ?? 'no one'}`;
-    }
-}
-const person = new Person();
-person.speak('Lucas');
-person.speak();
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-class Person {
-  speak(name?: string) {
-    return `Hello, ${name?.toUpperCase() ?? 'no one'}`
-  }
-}
-
-const person = new Person()
-person.speak('Lucas')
-person.speak()
+console.log('Hello world')
